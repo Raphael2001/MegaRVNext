527ab8cc570bcbd69e4f77ad47409c52c8389005
Raphael Aboohi <raphaelaboohi@gmail.com>
2025-09-20 19:59:22 -0700

    add "/"

--- src/app/[locale]/(site)/dynamic/[...route]/page.tsx
+++ src/app/[locale]/(site)/dynamic/[...route]/page.tsx
@@ -23,7 +23,7 @@ export default async function DynamicPage(props: Props) {
 
 	setRequestLocale(locale);
 
-	const fullRoute = route.join("/");
+	const fullRoute = "/" + route.join("/");
 
 	const payload = {
 		route: fullRoute,
